ed1af7ff2cea9a4f48e762b62ffea6c76e431e2a
Amakarena <ihejirika04@gmail.com>
2025-03-30 22:52:52 -0400

    test matrix

--- strassen.py
+++ strassen.py
@@ -1,8 +1,18 @@
 # Let's use a single source file to upload the progset
+import numpy as np
+
+#specify the size of the matrix
+n = 4
+
+#creates an n*n matrix 
+matrix = np.zeros((n, n), dtype=int)
+print(matrix)
+
+# A and B are nxn matrices (how to handle odd case?)
+# def conventional(A, B):
+
 
 # def strassen(n_matrix):
-
-# def standard(n_matrix):
 
 # def recursive_matrix_mult(n_matrix):
 
